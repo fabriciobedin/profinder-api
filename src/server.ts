b6c8a944b1e0f9404d608f2e23c0e8d6cd0b00c8
Fabricio Bedin <fabricioobedin@gmail.com>
2020-10-28 15:09:46 -0300

    Adding appointments route

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from 'express';
-import routes from './routes'
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  
-  return res.json({message: 'test'})
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('server started');
-  
-})
-
+});

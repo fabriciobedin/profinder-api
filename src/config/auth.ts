75bf9d09fb67abe5fce58b10cf2858fe448193df
Fabricio Bedin <fabricioobedin@gmail.com>
2020-10-31 19:34:06 -0300

    Creating middleware to require authentication in some routes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '3389dae361af79b04c9c8e7057f60cc6',
+    expiresIn: '30d'
+  }
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '3389dae361af79b04c9c8e7057f60cc6', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '30d'
+      expiresIn
     });
 
     return {

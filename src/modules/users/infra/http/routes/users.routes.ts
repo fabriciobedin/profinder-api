9756a2822e4faef38e35461be6d86619e7063008
Fabricio Bedin <fabricioobedin@gmail.com>
2020-11-19 02:03:23 -0300

    Adding route parameters validations with celebrate

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,17 +1,30 @@
 import { Router } from 'express';
 import multer from 'multer';
-
 import uploadConfig from '@config/upload';
-import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import UsersController from '@modules/users/infra/http/controllers/UsersController';
-import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', usersController.create);
+const upload = multer(uploadConfig);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required()
+    }
+  }),
+  usersController.create
+);
 
 usersRouter.patch(
   '/avatar',
